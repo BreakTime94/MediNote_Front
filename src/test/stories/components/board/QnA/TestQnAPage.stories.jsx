c09a30548395b3a1112889b85d5045a2ff07ea57
kys <tiger2ys98@gmail.com>
2025-09-25 16:05:52 +0900

    qna 목록 출력 및 등록 테스트 용 컴포넌트 생성

---
+++ src/test/stories/components/board/QnA/TestQnAPage.stories.jsx
@@ -0,0 +1,15 @@
+
+export default{
+    title: "Test//board/notifiy",
+    component: null,
+    parameters: {
+        reactRouterPath: "/notifiy",
+        reactExtraPath : [
+            {path: "/notifiy/register", element: null},
+            {path: "/notifiy/modify", element: null},
+            {path: "/notifiy/read", element: null}
+        ]
+    }
+};
+
+export const Board = () => null
