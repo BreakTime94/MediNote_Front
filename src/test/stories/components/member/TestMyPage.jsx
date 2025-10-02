6541edc48c5d0a43f6c6ad14b925c440fde8ad11
BreakTime94 <kimchan9408@gmail.com>
2025-10-02 18:15:52 +0900

    251002 member 회원가입 컴포넌트 정리, 소셜 회원가입 다듬기, 회원 개인정보 수정 기능 작성 중

--- src/test/stories/components/member/TestMyPage.jsx
+++ src/test/stories/components/member/TestMyPage.jsx
@@ -2,8 +2,9 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import LogoutButton from "./LogoutButton.jsx";
 import {useNavigate} from "react-router-dom";
-import {show} from "../common/ui/toast/commonToast.jsx";
 import api from "./axiosInterceptor.js";
+import ExtraEmailVerification from "./ExtraEmailVerification.jsx";
+import UseDuplicateCheck from "./UseDuplicateCheck.jsx";
 
 function TestMyPage() {
   const [memberDto, setMemberDto] = useState({
@@ -14,16 +15,51 @@ function TestMyPage() {
     regDate: ""
   });
   //인증여부
-  const[member, setMember] = useState({});
   const [verification, setVerification] = useState(false);
-  //인증코드
-  const [verificationCode, setVerificationCode] = useState(""); // 사용자가 입력한 코드
-  //코드창 보여주기
-  const [showCodeInput, setShowCodeInput] = useState(false);
+
   //읽기전용인지 아닌지(Mypage Component에서 수정도 같이 진행)
   const[readOnly, setReadOnly] = useState(true);
+  //최초 focus 여부
+  const[touched, setTouched] = useState({
+    extraEmail: false,
+    nickname: false,
+  })
+  //유효성 검사 실패내용 담음
+  const [errors, setErrors] = useState({});
+
+  const handleBlur = (e) => {
+    const { name, value } = e.target;
+    setTouched((prev) => ({ ...prev, [name]: true }));
+    setErrors((prev) => ({ ...prev, [name]: validation(name, value) }));
+  };
+
+  const validation = (name, value) => {
+    switch (name) {
+      case "extraEmail":
+        if (!value) return "추가 이메일은 필수입니다.";
+        if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)) {
+          return "올바른 이메일 형식이어야 합니다. ex) aaa@bbb.ccc 등";
+        }
+        break;
+
+      case "nickname":
+        if (!value) return "닉네임은 필수입니다.";
+        if (!/^[가-힣a-zA-Z0-9]{2,16}$/.test(value)) {
+          return "닉네임은 한글, 영문, 숫자만 사용하여 2~16자까지 가능합니다.";
+        }
+        break;
+      default:
+        return "";
+    }
+    return "";
+  }
+
+  const extraEmailStatus = UseDuplicateCheck("email", memberDto.extraEmail, "/member/check/email", validation);
+  const nicknameStatus = UseDuplicateCheck("nickname", memberDto.nickname, "/member/check/nickname", validation);
+
   //라우터 이동
   const navigate = useNavigate();
+
 
   useEffect(() => {
     axios.get("/api/member/get", {
@@ -52,6 +88,11 @@ function TestMyPage() {
     })
   }
 
+  const change = (e) => {
+    const {name, value} = e.target;
+    e.preventDefault();
+    setMemberDto((prev) => ({...prev, [name]: value}))
+  }
 
   const updateMember = (e) => {
     e.preventDefault();
@@ -63,49 +104,6 @@ function TestMyPage() {
     setReadOnly(true);
   }
 
-  // 인증메일 요청 (then/catch)
-  const handleSendVerificationCode = () => {
-    api
-        .post("/member/email/send", null, {
-          params: {email: member.email}
-        })
-        .then((resp) => {
-          show.success({
-                title : resp.data.message,
-                desc: resp.data.desc
-              }
-          );
-          setShowCodeInput(true);
-        })
-        .catch(() => {
-          show.error({
-            title : "인증 메일 발송에 실패했습니다.",
-            desc: "잠시 후 다시 시도하여주시기 바랍니다."}
-          );
-        });
-  };
-
-  // 인증코드 확인 (then/catch)
-  const handleVerifyCode = () => {
-    api
-        .post("/member/email/verify", {
-          email: member.email,
-          code: verificationCode,
-        })
-        .then((res) => {
-          if (res.data.available === true) {
-            show.success({title: res.data.message});
-            setVerification(true);
-            setShowCodeInput(false);
-          } else {
-            show.formerr({title: "인증 코드가 올바르지 않습니다."});
-          }
-        })
-        .catch(() => {
-          show.error({title: "인증 확인 중 오류가 발생했습니다."});
-        });
-  };
-
   return(
       <>
         {memberDto &&
@@ -113,76 +111,33 @@ function TestMyPage() {
           <div className={"gap-2 w-96 border-1"}>
             <div className={"m-2"}>
               <label className="block text-sm font-medium text-gray-700 mb-1">아이디</label>
-              <input className={"border w-full"} name={"email"} id={"name"} value={memberDto.email} readOnly={true} />
+              <input className={"border w-full"} name={"email"} id={"name"} value={memberDto.email} readOnly={true}/>
             </div>
             <div className={"m-2"}>
               <label className="block text-sm font-medium text-gray-700 mb-1">닉네임</label>
-              <input className={"border w-full"} value={memberDto.nickname} readOnly={readOnly}/>
+              <input className={"border w-full"} name={"nickname"} value={memberDto.nickname} readOnly={readOnly} onChange={change}/>
             </div>
-            <div className={"m-2"}>
-              <label className="block text-sm font-medium text-gray-700 mb-1">추가이메일</label>
-              <input className={"border w-full"} value={memberDto.extraEmail} readOnly={readOnly}/>
-              {/* 인증 메일 요청 버튼 */}
-              {!readOnly && !verification && (
-                  <button
-                      type="button"
-                      className="mt-2 w-full px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-                      onClick={handleSendVerificationCode}>
-                    인증메일 요청
-                  </button>
-              )}
-
-              {/* 인증 코드 입력창 + 확인 버튼 */}
-              {!readOnly && showCodeInput && !verification && (
-                  <div className="mt-2 flex space-x-2">
-                    <input
-                        type="text"
-                        name="verificationCode"
-                        placeholder="인증코드 입력"
-                        className="flex-1 px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400  disabled:bg-gray-200"
-                        disabled={verification}
-                        value={verificationCode}
-                        onChange={(e) => setVerificationCode(e.target.value)}
-                    />
-                    <button
-                        type="button"
-                        className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 disabled:bg-amber-100"
-                        disabled={verification}
-                        onClick={handleVerifyCode}
-                    >
-                      확인
-                    </button>
-                  </div>
-              )}
-              {!showCodeInput && verification && (
-                  <ul className="mt-2 text-xs">
-                    <li className="text-blue-500">이메일 인증이 완료되었습니다.</li>
-                  </ul>
-              )}
-              {member.extraEmail && member.email === member.extraEmail &&
-                  (<ul className={"mt-2 text-xs"}>
-                    <li className={"text-red-500"}>추가 이메일과 같은 이메일을 사용하실 수 없습니다.</li>
-                  </ul>)}
-            </div>
+            <ExtraEmailVerification member={memberDto} touched={touched} errors={errors} emailStatus={extraEmailStatus} handleBlur={handleBlur}
+            change={change} verification={verification}/>
             <div className={"m-2"}>
               <label className="block text-sm font-medium text-gray-700 mb-1">프로필이미지</label>
-              <input className={"border w-full"} value={memberDto.profileImagePath || "이미지가 없습니다."} readOnly={readOnly}/>
+              <input className={"border w-full"} name={"profileImagePath"} value={memberDto.profileImagePath || "이미지가 없습니다."} readOnly={true}/>
             </div>
             <div className={"m-2"}>
               <label className="block text-sm font-medium text-gray-700 mb-1">계정등록일</label>
-              <input className={"border w-full"} value={memberDto.regDate} readOnly={true}/>
+              <input className={"border w-full"} name={"regDate"} value={memberDto.regDate} readOnly={true}/>
             </div>
             {readOnly && (<div className={"m-2"}>
               <LogoutButton children = {"로그아웃"} onClick={logout} />
             </div>)}
-            <div  className={"m-2"}>
+            <div  className={"m-2 text-sm"}>
               {readOnly &&
-                (<button type={"button"} className="flex-1 bg-purple-300 text-gray-700 py-2 rounded-lg shadow
+                (<button type={"button"} className="flex-1 bg-purple-300 text-gray-700 px-2 py-1 rounded-lg shadow
                      hover:bg-purple-400 active:bg-purple-500 cursor-pointer" onClick={updateMember}>
                 수정하기
               </button>)}
               {!readOnly &&
-                  (<button type={"button"} className="flex-1 bg-purple-300 text-gray-700 py-2 rounded-lg shadow
+                  (<button type={"button"} className="flex-1 bg-purple-300 text-gray-700 px-2 py-1 rounded-lg shadow
                      hover:bg-purple-400 active:bg-purple-500 cursor-pointer" onClick={updateDone}>
                     수정완료
                   </button>)}
