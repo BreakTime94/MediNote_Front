7580634b94fc27fe59fdeb3ac0d5174b5781d5dc
kimheeseon0526 <khseon0526@naver.com>
2025-10-14 14:32:52 +0900

    251014 뒤로가기 버튼 class 변경

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -56,7 +56,7 @@ export default function MyPage() {
                     setEditId(null);
                     setSelectedData(null);
                   }}
-                  className="mb-4 px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-700 rounded-lg shadow-sm transition"
+                  className="mb-4 px-4 py-2 text-gray-700 transition"
               >
                 ← 뒤로가기
               </button>
