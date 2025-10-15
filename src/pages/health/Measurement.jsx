5da9027df8ac9d74b209dcedee76b2f685ea5ad0
kimheeseon0526 <xuan6.6j@gmail.com>
2025-10-16 00:10:32 +0900

    251015 태그 계층 정리

--- src/pages/health/Measurement.jsx
+++ src/pages/health/Measurement.jsx
@@ -544,7 +544,7 @@ function Measurement() {
                 name="bloodSugar"
                 value={form.bloodSugar || ""}
                 onChange={handleChange}
-                placeholder="20"
+                placeholder="90"
                 className={`border rounded-lg px-3 py-2 w-full ${errors.bloodSugar ? "border-red-400" : ""}`}
               />
               {errors.bloodSugar && (<p className="text-red-500 text-sm mt-1">{errors.bloodSugar}</p>)}
